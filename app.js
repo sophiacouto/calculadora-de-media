8e1566cd56858c84120a1443b24b89df8c5b9345
sophiacouto <sophiacouto.rolo@gmail.com>
2026-03-02 10:21:43 -0300

    estamos quase lá

---
+++ app.js
@@ -0,0 +1,13 @@
+const numero1 = document.getElementById("nota1")
+const numero2 = document.getElementById("nota2")
+const button = document.getElementById("button")
+const resultado = document.getElementById("resultado")
+
+function calculaMedia(){
+    const nota1 = Number =(numero1.value)
+    const nota2 = Number =(numero2.value)
+
+    const media = (nota1 + nota2)/2
+    resultado.innerTex = media
+}
+button.addEventListener("click", calculaMedia)
